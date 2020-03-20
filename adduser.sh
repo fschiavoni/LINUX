f03c505680e0d8de5bfd6a0b4ec753d472a5b9dd
Facundo Schiavoni <fschiavo@ucm.es>
2020-03-20 01:59:47 +0100

    adduser.sh
    This script lets you create all the users and groups in a simple way.

---
+++ adduser.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Script to add a user to Linux system
+
+while [ true ]
+do
+	#################	ADD USER 	#################
+	while [ true ]
+	do
+	# Si el usuario es root, entramos en el if
+	if [ $(id -u) -eq 0 ]; then 
+
+### Hacemos input del usuario y su contraseña
+#   Si el usuario introduce "0" se termina el script.
+	read -p "Enter username : " username
+
+		if [ "$username" = "0" ]; then break
+		fi
+
+	read -s -p "Enter password : " password
+
+### Buscamos dicho usuario en la carpeta passwd
+	egrep "^$username" /etc/passwd >/dev/null
+
+### Comprobarmos que el usuario no existe
+
+### $? equivale al return del ultimo comando utilizado
+#	en este caso, egrep. Si egrep devuelve 0 es que ha
+### encontrado al usuario por tanto ya existe.
+		if [ $? -eq 0 ]; then
+			echo "$username already exists!"
+			exit 1
+		else 
+			useradd -m -p $password $username
+			[ $? -eq 0 ] && echo "User - $username - has been added to the system." ||
+			echo "Failed to add the user - $username -"
+		fi
+
+	else 
+		echo "Only root may add a user to the system"
+		exit 2	
+	fi
+done 
+    #################	ADD GROUP 	################
+	read -p "Enter group name : " group
+
+	if [ "$group" = "0" ]; then break
+	fi
+
+	egrep "^$group" /etc/group >/dev/null
+
+	if [ $? -eq 0 ]; then
+		echo "$group already exists!"
+		exit 1
+	else 
+
+	groupadd $group
+	[ $? -eq 0 ] && echo "Group - $group - has been added to the system." ||
+	echo "Failed to add the group - $group -"
+	fi
+
+
+done
